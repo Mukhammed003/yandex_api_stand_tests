7cc90419fec58ae2f231e572a8ac1aaa45cab0b9
Mukhammed <Muhammed.Nurmuhanov@mail.ru>
2024-02-14 20:02:01 +0600

    My first commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://754f3bd0-4c19-4adf-a589-9105d90c62f4.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
